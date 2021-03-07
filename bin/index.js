abec4bb7ef2b2a7ce6d85cd605fb9199f660396d
swimly <979741120@qq.com>
2021-03-07 11:27:13 +0800

    add doc

--- bin/index.js
+++ bin/index.js
@@ -3,30 +3,38 @@ const fs = require('fs');
 const program = require('commander');
 const download = require('download-git-repo');
 const inquirer = require('inquirer');
+const handlebars = require('handlebars')
 const ora = require('ora');
 const chalk = require('chalk');
 const logSymbols = require('log-symbols');
 const pkg = require('../package.json')
 
-function init (name) {
-  inquirer.prompt({
+const init = (name) => {
+  inquirer.prompt([{
+    name: 'description',
+    message: '请输入项目描述！'
+  }, {
+    name: 'author',
+    message: '请输入作者名称'
+  }, {
     type: 'list',
     name: 'template',
     message: '请选择模板：',
     choices: ['rollup', 'vue']
-  }).then(res => {
+  }]).then(res => {
     const template = res.template
     console.log(logSymbols.warning, `您选择了[${chalk.green(template)}]模板，即将开始拉取模板！`)
     const spinner = ora('正在拉取中……');
     spinner.start()
     var start = (new Date()).getTime()
-    download(`swimly/${template}-template`, name, {clone: false}, (err) => {
+    download(`direct:https://codeload.github.com/swimly/${template}-template/zip/master`, name, { clone: false }, (err) => {
       if (err) {
         spinner.fail('失败')
         console.log(err)
       } else {
-        const distime = ((new Date()).getTime() - start)/1000
+        const distime = ((new Date()).getTime() - start) / 1000
         spinner.succeed(`远程模板拉取成功！`)
+        rewritePackage(res, name)
         console.log(logSymbols.success, `总耗时：${distime}s`)
         console.log('执行如下命令：')
         console.log(logSymbols.success, `cd ${name}`)
@@ -36,21 +44,36 @@ function init (name) {
   })
 }
 
-program.version(pkg.version, '-v, --version')
-.command('init <name>')
-.action((name) => {
-  if (!fs.existsSync(name)) {
-    init(name)
-  } else {
-    inquirer.prompt({
-      type: 'confirm',
-      name: 'cover',
-      message: `${name}目录已存在，请确认是否要覆盖？`
-    }).then(res => {
-      if (res.cover) {
-        init(name)
-      }
-    })
+const rewritePackage = (res, name) => {
+  const fileName = `${name}/package.json`;
+  const meta = {
+    name,
+    description: res.description,
+    author: res.author
   }
-})
+  if (fs.existsSync(fileName)) {
+    const content = fs.readFileSync(fileName).toString();
+    const result = handlebars.compile(content)(meta);
+    fs.writeFileSync(fileName, result);
+  }
+  console.log(chalk.green('success'));
+}
+
+program.version(pkg.version, '-v, --version')
+  .command('init <name>')
+  .action((name) => {
+    if (!fs.existsSync(name)) {
+      init(name)
+    } else {
+      inquirer.prompt({
+        type: 'confirm',
+        name: 'cover',
+        message: `${name}目录已存在，请确认是否要覆盖？`
+      }).then(res => {
+        if (res.cover) {
+          init(name)
+        }
+      })
+    }
+  })
 program.parse(process.argv)
---
+++ readme.md
@@ -0,0 +1,13 @@
+## 使用方法
+
+### 1、全局安装
+
+``` bash
+npm i -g @swimly/longrise-cli
+```
+
+### 2、新建项目
+
+``` bash
+longrise init {{name}}
+```
